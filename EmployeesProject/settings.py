04db2c1d2b789852656d6d104e80f1ac1720b702
Kirill Shkrogalev <kirill.shkrogalev@gmail.com>
2021-12-07 18:42:36 +0300

    Work complete done

--- EmployeesProject/settings.py
+++ EmployeesProject/settings.py
@@ -74,19 +74,15 @@ WSGI_APPLICATION = 'EmployeesProject.wsgi.application'
 
 DATABASES = {
 
-    # 'default': {
-    #  'ENGINE': 'django.db.backends.postgresql_psycopg2',
-    #  'NAME': 'test_employee',
-    #  'USER': 'postgres',
-    #  'PASSWORD': 'postgres',
-    #  'HOST': '127.0.0.1',
-    #  'PORT': '5432',
-    # }
-
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3'
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'test_employee',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
     }
+
 }
 
 # Password validation
